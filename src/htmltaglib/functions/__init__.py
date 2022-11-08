ac2e7428e02f17200696f7829c99857ac10a2258
Micah <micahjhanevich@gmail.com>
2022-11-08 16:48:28 -0500

    finalized import statements and created function stub

---
+++ src/htmltaglib/functions/__init__.py
@@ -0,0 +1,1 @@
+from .import_html import import_html
--- src/htmltaglib/functions/import_html.py
+++ src/htmltaglib/functions/import_html.py
@@ -1,2 +1,22 @@
-def import_html():
-    pass
+from _io import TextIOWrapper
+import typing
+from .. import *
+
+
+def import_html(f: [TextIOWrapper, str]) -> Element:
+    """
+    Import a raw page / file of HTML and return a collection of HTMLTagLib Elements
+    :param f: Can be a `TextIOWrapper` passed from the builtin `open` function; a str with the filepath of the file; or a str containing an html page.
+    :return: Element
+    """
+
+    if type(f) == TextIOWrapper:
+        print('Wrapper!')
+    else:
+        print('Not that!')
+
+    return HTML()
+
+
+import_html(open('text.txt'))
+import_html('text.txt')
